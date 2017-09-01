6580584a2af6cfdc820f0522a929ef3914c1ad2d
Hugh Morrison <hmorri32@gmail.com>
2017-08-31 20:08:11 -0600

    build accessors and add fired_on to initialize

--- lib/space.rb
+++ lib/space.rb
@@ -1,7 +1,11 @@
 class Space
+  attr_accessor :coordinates, 
+                :occupied,
+                :fired_on
   
   def initialize(coordinates)
     @coordinates = coordinates
     @occupied    = false
+    @fired_on    = false
   end
 end
