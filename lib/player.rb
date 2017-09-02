6e739d1e48da88ab61d53d75091c3297a64a7c2e
Hugh Morrison <hmorri32@gmail.com>
2017-09-02 15:08:06 -0600

    initialize with ships, supplementary methods

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,23 @@
-class Player
-  attr_accessor :shot_count
+require_relative 'ship'
 
-  def initialize
+class Player
+  attr_accessor :shot_count, :ship_arr
+
+  def initialize(ship_arr = nil)
     @shot_count = 0
+    @ship_arr   = ship_arr
+    @ships      = []
+  end
+
+  def shoot 
+    @shot_count +=1 
+  end
+
+  def ship_builder 
+    @ships = @ship_arr.map {|length| Ship.new(length)}
+  end
+  
+  def total_ships
+    @ships.length
   end
 end
