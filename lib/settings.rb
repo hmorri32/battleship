85a24023428ad1ee48d0cc9b0bd770910ae648a9
Hugh Morrison <hmorri32@gmail.com>
2017-09-02 15:08:29 -0600

    touch settings, build hash to determine ship lenghts

--- lib/settings.rb
+++ lib/settings.rb
@@ -1,10 +1,12 @@
 class Settings
   def initialize
-    
+    @beginner     = {'size' => 4,  'ships' => [2,3]}
+    @intermediate = {'size' => 8,  'ships' => [2,3,4]}
+    @advanced     = {'size' => 12, 'ships' => [2,3,4,5]}
   end
 end
 
 # beginner, intermediate, advanced
 # beginner     => size 4  => ships 2, 3
 # intermediate => size 8  => ships 2, 3, 4
-# advanced     => size 12 => ships 2, 3, 4,5
+# advanced     => size 12 => ships 2, 3, 4, 5
