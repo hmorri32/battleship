8aedcd0323c61200bec4252a9c3d5724796fff21
Hugh Morrison <hmorri32@gmail.com>
2017-09-02 14:41:15 -0600

    pseudocode settings

---
+++ lib/settings.rb
@@ -0,0 +1,10 @@
+class Settings
+  def initialize
+    
+  end
+end
+
+# beginner, intermediate, advanced
+# beginner     => size 4  => ships 2, 3
+# intermediate => size 8  => ships 2, 3, 4
+# advanced     => size 12 => ships 2, 3, 4,5
