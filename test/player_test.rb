b6adfe500ab9f7a7aace570012f618a794c0ef97
Hugh Morrison <hmorri32@gmail.com>
2017-09-02 15:09:15 -0600

    test instantiated ship methods in player

--- test/player_test.rb
+++ test/player_test.rb
@@ -2,17 +2,41 @@ require_relative 'require'
 require './lib/player'
 
 class PlayerTest < Minitest::Test 
+  attr_accessor :player, :playa
   def setup
     @player = Player.new 
+    @playa  = Player.new([2,3])
   end
 
   def test_existence 
-    assert @player
-    assert_instance_of Player, @player
+    assert player
+    assert_instance_of Player, player
   end
 
   def test_shot_count
-    assert_equal 0, @player.shot_count
+    assert_equal 0, player.shot_count
+  end
+
+  def test_default_ships 
+    assert_nil player.ship_arr
+    assert_equal [2,3], playa.ship_arr
+  end
+
+  def test_total_ship_count 
+    playa.ship_builder
+    assert_equal 2, playa.total_ships 
+  end
+
+  def test_playa_can_shoot 
+    assert_equal 0, playa.shot_count
+    
+    playa.shoot
+
+    assert_equal 1, playa.shot_count
+  end
+
+  def test_playa_can_place_ship 
+    
   end
 end
 
