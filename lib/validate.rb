6a69f21929b73bebac8ca2269a631bb0d027048f
Hugh Morrison <hmorri32@gmail.com>
2017-09-03 13:14:39 -0600

    delete edge of the world validation

--- lib/validate.rb
+++ lib/validate.rb
@@ -11,10 +11,6 @@ module Validate
     board.space_exists?(space_1) && board.space_exists?(space_2)
   end
 
-  def small_ship_stays_on_map?(board, space_1, space_2)
-    board.neighbors?(space_1, space_2)
-  end
-
   def no_diagonals?(space_1, space_2)
     space_1[0] == space_2[0] || space_1[1] == space_2[1]
   end
