15a115a3fb57742f384e08c6f562b43583045fa6
Hugh Morrison <hmorri32@gmail.com>
2017-09-02 18:03:31 -0600

    edge of the world validation method

--- lib/validate.rb
+++ lib/validate.rb
@@ -6,4 +6,12 @@ module Validate
   def valid_space?(board, space)
     board.space_exists?(space)
   end
+
+  def two_valid_spaces?(board, space_1, space_2)
+    board.space_exists?(space_1) && board.space_exists?(space_2)
+  end
+
+  def small_ship_stays_on_map?(board, space_1, space_2)
+    board.neighbors?(space_1, space_2)
+  end
 end
