e318877bdd062064becb7a594bfcf8a5e3a904b3
Hugh Morrison <hmorri32@gmail.com>
2017-09-02 18:57:31 -0600

    length validation

--- lib/validate.rb
+++ lib/validate.rb
@@ -18,4 +18,12 @@ module Validate
   def no_diagonals?(space_1, space_2)
     space_1[0] == space_2[0] || space_1[1] == space_2[1]
   end
+
+  def valid_length?(board, ship, space_1, space_2)
+    if board.same_row?(space_1, space_2)
+      ship.length == board.span_horizontally(space_1, space_2)
+    else
+      ship.length == board.span_vertically(space_1, space_2)
+    end
+  end
 end
