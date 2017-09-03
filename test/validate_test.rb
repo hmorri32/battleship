e793a714487c181e7f79ffd4f2c933e38fcf9785
Hugh Morrison <hmorri32@gmail.com>
2017-09-03 13:14:21 -0600

    delte redundant test

--- test/validate_test.rb
+++ test/validate_test.rb
@@ -14,7 +14,7 @@ class ValidateTest < Minitest::Test
     assert_equal 'ultra chill', ultra_chill
   end
 
-  def test_valid_space 
+  def test_valid_space
     assert valid_space?(@board, 'A4')
     assert valid_space?(@board, 'A1')
     assert valid_space?(@board, 'A2')
@@ -32,15 +32,6 @@ class ValidateTest < Minitest::Test
 
     refute two_valid_spaces?(@board, 'A1', 'Z2')
     refute two_valid_spaces?(@board, 'A1', 'M2')
-  end
-
-  def test_small_ship_doesnt_fall_off_map 
-    assert small_ship_stays_on_map?(@board, 'A3', 'A4')
-    assert small_ship_stays_on_map?(@board, 'A4', 'B4')
-    assert small_ship_stays_on_map?(@board, 'C2', 'D2')
-
-    refute small_ship_stays_on_map?(@board, 'A4', 'A5')
-    refute small_ship_stays_on_map?(@board, 'D4', 'E4')
   end
 
   def test_no_diagonals 
