45c17540add62b87ed10db4f889f03993d1e1833
Hugh Morrison <hmorri32@gmail.com>
2017-09-04 11:34:43 -0600

    pseudoocode dashboard

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -1,16 +1,80 @@
+require 'colorize'
+require_relative 'board'
+require_relative 'ship'
+require_relative 'player'
+
 class DashBoard 
-  def method_name
-    
+
+  def border
+    '=============='   
   end
-  def method_name
-    
+
+  def format_row(arr)
+    arr.map {|letter| letter == '' ? ' ' : letter}.join(' ')
   end
-  def method_name
-    
+
+  def top(size, arr = ["."])
+    for i in 1..size do 
+      arr << i.to_s
+    end
+    arr.join(' ')
   end
-  def method_name
-    
+
+  def grid(size, rows)
+    "#{border}\n#{top(size)}\n#{rows.map {|letter|"#{format_row(letter)}\n"}.join}#{border}"
+  end
+
+  def rows(board)
+    board.board.map do |row|
+      arr = []
+      row.each do |s|
+        space = s.values[0]
+        arr.push(space.position[0]) if space.position[1] == "1"
+        arr.push("")  if empty?(board, space.position)
+        arr.push("H") if hit?(board, space.position)
+        arr.push("M") if miss?(board, space.position)
+      end
+      arr
+    end
+  end
+
+  def render(board)
+    grid(board.size, rows(board))
+  end
+
+  def hit?(board, space)
+    board.hit?(space)
+  end
+
+  def miss?(board, space)
+    board.miss?(space)
+  end
+
+  def empty?(board, space)
+    board.fired_on?(space) == false
   end
 end
 
-# TODO BUILD THE REST OF THE FOOKIN BOARD 
+
+# board = Board.new(4)
+# # puts 'cool'.colorize(:red)
+# # TODO BUILD THE REST OF THE FOOKIN BOARD 
+# dash = DashBoard.new
+# # p dash.border(4)
+# # p dash.row(['', 'M'])
+# row1 = ['A', 'H', '', '', 'M']
+# row2 = ['B', '', '', '', 'H']
+# row3 = ['C', '', '', '', 'H']
+# row4 = ['D', 'H', 'H', '', '']
+# # puts dash.grid(4, row1, row2, row3, row4)
+# # p dash.get_rows_from_board(board)
+# board.fire_on('A1')
+# board.fire_on('B3')
+# board.fire_on('A3')
+# player = Player.new([2,3])
+# ship = player.ships[0]
+# player.place_ship(board, ship, 'A4', 'B4')
+# player.shoot(board, 'A4')
+# player.shoot(board, 'B4')
+# player.shoot(board, 'C4')
+# puts dash.render(board)
