3bd48f75055b8bc59d2b9e0e5c85534619fa3997
Hugh Morrison <hmorri32@gmail.com>
2017-09-04 12:23:48 -0600

    initialize computer player

--- lib/computer.rb
+++ lib/computer.rb
@@ -7,10 +7,16 @@ class Computer < Player
     board.space_name_arr.sample
   end
 
-  
+  def pick_spaces(board, history = [])
+    spaces = board.space_name_arr
+    until spaces.length == 0 do
+      spaces.pop
+    end
+
+  end
 
 end
 
 board = Board.new(4)
 c = Computer.new([2,3])
-p c.random_space(board)
+p c.pick_spaces(board)
