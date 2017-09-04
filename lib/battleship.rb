59fbd89e11d4d9845f52a2d4ad977b2a2b08eaef
Hugh Morrison <hmorri32@gmail.com>
2017-09-04 15:52:23 -0600

    place ship sequence

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -8,6 +8,7 @@ require 'colorize'
 
 class BattleShip
   include Messages 
+  include Validate
 
   attr_accessor :player, 
                 :computer, 
@@ -19,9 +20,7 @@ class BattleShip
     @computer       = Computer.new([2,3])
     @computer_board = Board.new(4)
     @player_board   = Board.new(4)
-
     ship_placement
-    # play
   end
 
   def ship_placement 
@@ -31,7 +30,7 @@ class BattleShip
   
   def computer_ships 
     computer.ships.each do |ship|
-      spaces = computer.coordinates(computer_board, ship)
+      spaces = computer.position(computer_board, ship)
       computer.place_ship(computer_board, ship, spaces[0], spaces[1])
     end
     puts Messages.computer_has_placed_ships.colorize(:red)
@@ -39,39 +38,65 @@ class BattleShip
 
   def player_ships 
     player.ships.each do |ship|
-      # spaces = gets.chomp
-      validate_input(ship)
-      # p ship
-      # player.place_ship(player_board, ship, spaces[0], spaces[1])
+      spaces = validate_input(ship)
+      player.place_ship(player_board, ship, spaces[0], spaces[1])
+      puts "\n You have placed your #{ship.length} unit ship at #{spaces[0]} #{spaces[1]}".colorize(:blue)
     end
   end
 
   def validate_input(ship)
-    valid = false 
-    
-    puts Messages.place_ship(ship) 
-    answer = gets.chomp.upcase.split(" ")
-    valid_space?(player_board, ship, answer)
+    dale = false 
+    until dale 
+      puts Messages.place_ship(ship) 
+      answer = gets.chomp.upcase.split(" ")
+      dale = valid_space?(player_board, ship, answer)
+    end
+    return answer
   end
 
   def valid_space?(board, ship, answer)
-    # on the map 
-    #  not diagonal 
-    # valid length 
-    # not overlapping 
-    
+    validity  = false
+    input     = valid_gets?(board, ship, answer)
+    direction = valid_direction?(answer)      if input
+    length    = length?(board, ship, answer)  if direction
+    validity  = overlap?(board, ship, answer) if length
+    return validity
   end
 
-  def play
-
-    # until game_over?(player, computer) do 
-    
-    p 'fook!'
+  def length?(board, ship, answer)
+    if !valid_length?(board, ship, answer[0], answer[1])
+      puts "\n Error, ship length invalid!".colorize(:red)
+      return false
+    end
+    return true
   end
 
-  def place_ships 
-
+  def overlap?(board, ship, answer)
+    if !no_overlap?(board, ship, answer[0], answer[1])
+      puts "\nThat space is already occupied!".colorize(:red)
+      return false
+    end
+    return true
   end
 
-  # game_runner
+  def valid_direction?(answer)
+    if !no_diagonals?(answer[0], answer[1])
+      puts "\n Error, No diagonal placement!".colorize(:red)
+      return false
+    end
+    return true
+  end
+
+  def valid_gets?(board, ship, answer)
+    if !two_valid_spaces?(board, answer[0], answer[1])
+      if answer.length < 2 
+        puts "\nError, Please input two spaces".colorize(:red)
+        return false 
+      else 
+        puts "\nError, Space Invalid.".colorize(:red)
+        return false
+      end
+    end
+    return true
+  end
 end
