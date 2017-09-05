a3116d2e8c61af5580e2b99c442ee29d60589927
Hugh Morrison <hmorri32@gmail.com>
2017-09-05 16:10:39 -0600

    delete pseudocode

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -54,27 +54,3 @@ class DashBoard
     board.fired_on?(space) == false
   end
 end
-
-
-# board = Board.new(4)
-# # puts 'cool'.colorize(:red)
-# # TODO BUILD THE REST OF THE FOOKIN BOARD 
-# dash = DashBoard.new
-# # p dash.border(4)
-# # p dash.row(['', 'M'])
-# row1 = ['A', 'H', '', '', 'M']
-# row2 = ['B', '', '', '', 'H']
-# row3 = ['C', '', '', '', 'H']
-# row4 = ['D', 'H', 'H', '', '']
-# # puts dash.grid(4, row1, row2, row3, row4)
-# # p dash.get_rows_from_board(board)
-# board.fire_on('A1')
-# board.fire_on('B3')
-# board.fire_on('A3')
-# player = Player.new([2,3])
-# ship = player.ships[0]
-# player.place_ship(board, ship, 'A4', 'B4')
-# player.shoot(board, 'A4')
-# player.shoot(board, 'B4')
-# player.shoot(board, 'C4')
-# puts dash.render(board)
